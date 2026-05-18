50ecbc748dd84d51d57398a94698134a2b293b65
nico <141409541+nichothenacho64@users.noreply.github.com>
2026-05-18 11:05:30 +1000

    Initialise website
    Currently, there is a very simple website with nothing but a test chart and applying a font to a chart

---
+++ website/script.js
@@ -0,0 +1,56 @@
+function addFonts(layout) {
+    const fontFamily = '"Source Sans 3", sans-serif';
+
+    return {
+        ...layout,
+        font: {
+            ...layout.font,
+            family: fontFamily
+        },
+        hoverlabel: {
+            ...layout.hoverlabel,
+            font: {
+                ...layout.hoverlabel?.font, // optional chaining is neeeded here to prevent accessing .font on undefined
+                family: fontFamily
+            }
+        }
+    }
+}
+
+// test chart
+var trace1 = {
+    x: [1, 2, 3, 4, 5],
+    y: [1, 6, 3, 6, 1],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'Team A',
+    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+    marker: { size: 12 }
+};
+
+var trace2 = {
+    x: [1.5, 2.5, 3.5, 4.5, 5.5],
+    y: [4, 1, 7, 1, 4],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'Team B',
+    text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
+    marker: { size: 12 }
+};
+
+var data = [trace1, trace2];
+
+var layout = {
+    xaxis: {
+        range: [0.75, 5.25]
+    },
+    yaxis: {
+        range: [0, 8]
+    },
+    title: { text: 'Homeless graduate test chart' },
+    font: {
+        color: "#eb4040",
+    }
+};
+
+Plotly.newPlot('testChart', data, addFonts(layout));
